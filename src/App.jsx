7e06887f0ec316eac58df1ba0ff90b8bb732fc91
Abhishek Verma <abhiiit63@gmail.com>
2025-10-07 18:19:42 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import TestSeriesTracker from './TestSeriesTracker'; 
+// Make sure TestSeriesTracker.jsx is in the same 'src' directory
+
+function App() {
+  // We are rendering only our custom component, which contains everything.
+  return (
+    <TestSeriesTracker />
+  );
+}
+
+export default App;
